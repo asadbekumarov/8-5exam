26dd73b17f5cdbcb0697059b791cf1a461abfa12
asadbek <asadbekumarov@gmail.com>
2025-02-28 14:27:43 +0500

    end

--- src/app/edu/page.tsx
+++ src/app/edu/page.tsx
@@ -44,6 +44,7 @@ const Edu = () => {
       <p className="text-gray-600 mt-2">
         🔧 Add any school or bootcamp that you have attended
       </p>
+      <div></div>
       <form onSubmit={Edu} className="mt-6">
         <input
           type="text"
