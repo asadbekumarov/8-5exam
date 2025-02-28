5203de26a1f13ec4b6ac4f2c5842ab593d9be1aa
asadbek <asadbekumarov@gmail.com>
2025-02-28 15:01:01 +0500

    end

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -72,6 +72,7 @@ function Dashboard() {
               </button>
             </div>
           </div>
+          
         ) : (
           <button className="bg-[#17a2b8] py-2 text-center flex justify-center px-4 text-white">
             <Link href="/createProfile">Create Profile</Link>
